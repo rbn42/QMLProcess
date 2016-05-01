f7b1239be8f692129058369b97241c51d39dd4d1
rbn42 <bl100@students.waikato.ac.nz>
2016-05-01 19:59:07 +1200

    launcher

--- launcher.cpp
+++ launcher.cpp
@@ -12,7 +12,11 @@ Launcher::Launcher(QObject *parent) :
 QString Launcher::launch(const QString &program,
         const QStringList &arguments)
 {
-    m_process->start(program,arguments);
+//    m_process->start(program,arguments);
+
+    QProcess* p=new QProcess(this);
+    p->start(program,arguments);
+//    delete p;
 //    m_process->waitForFinished(0);
    // m_process->waitForFinished(-1);
    // QByteArray bytes = m_process->readAllStandardOutput();
