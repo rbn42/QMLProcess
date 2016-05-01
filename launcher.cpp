1552e78afec3dfb747f37b2474d7f1dbadcf0dd1
rbn42 <bl100@students.waikato.ac.nz>
2016-05-01 20:01:38 +1200

    ratio

--- launcher.cpp
+++ launcher.cpp
@@ -12,15 +12,15 @@ Launcher::Launcher(QObject *parent) :
 QString Launcher::launch(const QString &program,
         const QStringList &arguments)
 {
-//    m_process->start(program,arguments);
+    m_process->start(program,arguments);
 
-    QProcess* p=new QProcess(this);
-    p->start(program,arguments);
-//    delete p;
-//    m_process->waitForFinished(0);
-   // m_process->waitForFinished(-1);
-   // QByteArray bytes = m_process->readAllStandardOutput();
-   // QString output = QString::fromLocal8Bit(bytes);
-    //return output;
     return 0;
+    //QProcess* p=new QProcess(this);
+    //p->start(program,arguments);
+    //    delete p;
+    //    m_process->waitForFinished(0);
+    // m_process->waitForFinished(-1);
+    // QByteArray bytes = m_process->readAllStandardOutput();
+    // QString output = QString::fromLocal8Bit(bytes);
+    //return output;
 }
