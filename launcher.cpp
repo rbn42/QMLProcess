ec68e6f7d4cd672c1cbeaa453d63ab31db530e0a
rbn42 <bl100@students.waikato.ac.nz>
2016-07-19 09:45:42 +1200

    clean up

--- launcher.cpp
+++ launcher.cpp
@@ -1,6 +1,3 @@
-
-
-
 #include "launcher.h"
 
 Launcher::Launcher(QObject *parent) :
@@ -8,18 +5,10 @@ Launcher::Launcher(QObject *parent) :
     m_process(new QProcess(this))
 {
 }
+
 QString Launcher::launch(const QString &program,
         const QStringList &arguments)
 {
     m_process->start(program,arguments);
-
     return 0;
-    //QProcess* p=new QProcess(this);
-    //p->start(program,arguments);
-    //    delete p;
-    //    m_process->waitForFinished(0);
-    // m_process->waitForFinished(-1);
-    // QByteArray bytes = m_process->readAllStandardOutput();
-    // QString output = QString::fromLocal8Bit(bytes);
-    //return output;
 }
