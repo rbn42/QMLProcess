5894a77b62b7d780c330ec76ab4f67d2a9b67245
rbn42 <bl100@students.waikato.ac.nz>
2016-07-23 13:09:46 +1200

    working directory

--- launcher.cpp
+++ launcher.cpp
@@ -1,14 +1,22 @@
 #include "launcher.h"
 
-Launcher::Launcher(QObject *parent) :
-    QObject(parent),
-    m_process(new QProcess(this))
+Launcher::Launcher(QObject* parent)
+    : QObject(parent)
+    , m_process(new QProcess(this))
 {
 }
 
-QString Launcher::launch(const QString &program,
-        const QStringList &arguments)
+QString Launcher::launch(const QString& program,
+    const QStringList& arguments)
 {
-    m_process->start(program,arguments);
+    m_process->start(program, arguments);
+    return 0;
+}
+
+QString Launcher::launch(const QString& program,
+    const QStringList& arguments, const QString workingDirectory)
+{
+    m_process->setWorkingDirectory(workingDirectory);
+    m_process->start(program, arguments);
     return 0;
 }
--- launcher.h
+++ launcher.h
@@ -4,15 +4,15 @@
 #include <QObject>
 #include <QProcess>
 
-class Launcher : public QObject
-{
+class Launcher : public QObject {
     Q_OBJECT
 public:
-    explicit Launcher(QObject *parent = 0);
-    Q_INVOKABLE QString launch(const QString &program,const QStringList &arguments);
+    explicit Launcher(QObject* parent = 0);
+    Q_INVOKABLE QString launch(const QString& program, const QStringList& arguments);
+    Q_INVOKABLE QString launch(const QString& program, const QStringList& arguments, const QString workingDirectory);
 
 private:
-    QProcess *m_process;
+    QProcess* m_process;
 };
 
 #endif // LAUNCHER_H
